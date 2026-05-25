c2713cea5fdd4a1813c14bf0e3c8c239b7b61581
Mario Reder <mario.reder@pm.me>
2026-05-25 11:40:28 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "All in One Dex!",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "false",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Rewards,Swap,Campaigns",
+  "VITE_ENABLED_MENUS": "Portfolio,Swap,Trading,Rewards",
   "VITE_CUSTOM_MENUS": "Fiat, https://www.srd.exchange/fiat",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
